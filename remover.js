a20fb2ee16a0aced3ed825dabeb8a53edf8a9805
0xLGG <72031534+0xLGG@users.noreply.github.com>
2021-02-14 05:01:47 -0500

    removing as well ads and the banner now with css instead of js!

--- remover.js
+++
@@ -1,8 +0,0 @@
-var inter = window.setInterval( function() {
-	var doc = document.getElementsByClassName("banners-wrap")[0];
-
-	if (doc) {
-		doc.hidden = true;
-		window.clearInterval(inter);
-	}
-}, 300);
